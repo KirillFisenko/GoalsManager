1c3053a9cfa70d3a841c91bd2c96d54288e4377b
KirillFisenko <79265463469@ya.ru>
2024-07-31 20:55:29 +0300

    Add Service

--- TasksManager/Controllers/GoalController.cs
+++ TasksManager/Controllers/GoalController.cs
@@ -1,46 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TasksManager.Model;
+using TasksManager.Services;
 
 namespace TasksManager.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class GoalController : ControllerBase
+    public class GoalController(GoalServices goalServices) : ControllerBase
     {
-        private static readonly List<Goal> goals = [];
-
         [HttpGet]
         public List<Goal> GetAll()
         {
-            return goals;
+            return goalServices.GetAll();
         }
 
         [HttpGet("{id}")]
         public Goal Get(int id)
         {
-            return goals.FirstOrDefault(goal => goal.Id == id);
+            return goalServices.Get(id);
         }
 
         [HttpPost]
         public void Add(Goal goal)
         {
-            goals.Add(goal);
+            goalServices.Add(goal);
         }
 
         [HttpDelete]
         public void Delete(int id)
         {
-            var goal = goals.FirstOrDefault(goal => goal.Id == id);
-            goals.Remove(goal);
+            goalServices.Delete(id);
         }
 
         [HttpPut]
         public void Update(int id, Goal newGoal)
         {
-            var goal = goals.FirstOrDefault(goal => goal.Id == id);
-            goal.Name = newGoal.Name;
-            goal.Description = newGoal.Description;
-            goal.Statuses = newGoal.Statuses;
+            goalServices.Update(id, newGoal);
         }
     }
 }
---
+++ TasksManager/Services/GoalServices.cs
@@ -0,0 +1,38 @@
+﻿using TasksManager.Model;
+
+namespace TasksManager.Services
+{
+    public class GoalServices
+    {
+        private static readonly List<Goal> goals = [];
+
+        public List<Goal> GetAll()
+        {
+            return goals;
+        }
+
+        public Goal Get(int id)
+        {
+            return goals.FirstOrDefault(goal => goal.Id == id);
+        }
+
+        public void Add(Goal goal)
+        {
+            goals.Add(goal);
+        }
+
+        public void Delete(int id)
+        {
+            var goal = goals.FirstOrDefault(goal => goal.Id == id);
+            goals.Remove(goal);
+        }
+
+        public void Update(int id, Goal newGoal)
+        {
+            var goal = goals.FirstOrDefault(goal => goal.Id == id);
+            goal.Name = newGoal.Name;
+            goal.Description = newGoal.Description;
+            goal.Statuses = newGoal.Statuses;
+        }
+    }
+}
